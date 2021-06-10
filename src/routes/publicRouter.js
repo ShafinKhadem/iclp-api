b33b94053fc8da6a2723c0fae3ebd83cbec369d6
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2021-06-10 12:32:27 +0600

    get rank and total score

--- src/routes/publicRouter.js
+++ src/routes/publicRouter.js
@@ -105,4 +105,30 @@ publicRouter.route("/best/:userid").get(
     }
 );
 
+publicRouter.route("/rank").get(
+
+    async (req, res, next) => {
+        const { params, query, body, user, file } = req;
+
+        const sql = SQL`
+            select *
+            from (
+                     select *, row_number() over () rank
+                     from (
+                              select user_id, sum(score) total_score
+                              from (
+                                       select user_id, max(score) score
+                                       from challenge_results
+                                       group by user_id, challenge_id) q1
+                              group by user_id
+                              order by total_score desc) q2) q3
+            `;
+        if (query.userid !== undefined) {
+            sql.append(`where user_id = ${query.userid}`);
+        }
+        sql.append(';');
+        jsonDBQuery(res, next, sql);
+    }
+);
+
 module.exports = publicRouter;
