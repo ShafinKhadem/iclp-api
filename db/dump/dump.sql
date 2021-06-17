ed0d697df2882189f7040c177bf0d2007eab2c58
mwashief <muwashief@gmail.com>
2021-06-17 19:35:20 +0600

    dump updated

--- db/dump/dump.sql
+++ db/dump/dump.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.3 (Ubuntu 13.3-0ubuntu0.21.04.1)
--- Dumped by pg_dump version 13.3 (Ubuntu 13.3-0ubuntu0.21.04.1)
+-- Dumped from database version 13.3 (Ubuntu 13.3-1.pgdg20.04+1)
+-- Dumped by pg_dump version 13.3 (Ubuntu 13.3-1.pgdg20.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -68,7 +68,7 @@ CREATE TABLE public.challenge_results (
     "time" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     score integer,
     opponent_id integer,
-    details character varying(255)
+    details character varying(511)
 );
 
 
@@ -132,7 +132,8 @@ CREATE TABLE public.challenges (
     content character varying(65535),
     category public.challenge_type,
     difficulty public.difficulty_type,
-    score integer
+    score integer,
+    "time" integer
 );
 
 
@@ -282,13 +283,13 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 COPY public.challenge_results (id, challenge_id, user_id, "time", score, opponent_id, details) FROM stdin;
 2	1	1	2021-05-31 00:01:12.773866	5	\N	\N
 1	1	1	2021-05-30 22:57:16.354623	10	\N	TLE/MLE/lost to opponent etc.?
-3	2	1	2021-06-07 04:28:51.660716	15	\N	\N
 4	3	1	2021-06-07 07:02:41.8656	5	\N	\N
 5	4	1	2021-06-07 07:02:41.8656	5	\N	\N
 6	1	2	2021-06-10 06:05:53.115075	5	\N	\N
 7	1	1	2021-06-10 06:31:44.485811	10	\N	\N
 8	1	3	2021-06-11 13:19:39.429722	5	\N	\N
 9	1	4	2021-06-11 13:25:31.322649	3	\N	\N
+24	2	1	2021-06-17 19:25:06.149994	11	\N	["K-Means",["In SGD, you have to run through all the samples in your training set for a single update of a parameter in each iteration."],["Depth of Tree"]]
 \.
 
 
@@ -316,11 +317,11 @@ COPY public.challenge_topics (challenge_id, topic_id) FROM stdin;
 -- Data for Name: challenges; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.challenges (id, title, content, category, difficulty, score) FROM stdin;
-2	MCQ_loops	problem with solutions maybe?	mcq	hard	15
-4	conditionals	whatever	code	easy	5
-3	Arrays	whatever	code	medium	10
-1	Multiplication	you will be given an integer n (1 < n < 100) as input. output n*5.	code	hard	20
+COPY public.challenges (id, title, content, category, difficulty, score, "time") FROM stdin;
+4	conditionals	whatever	code	easy	5	\N
+3	Arrays	whatever	code	medium	10	\N
+1	Multiplication	you will be given an integer n (1 < n < 100) as input. output n*5.	code	hard	20	\N
+2	mcq sample	{"questions":[{"type":1,"points":5,"statement":"Which of the following is an example of a deterministic algorithm?","options":["PCA","K-Means","None of the above"],"answer":"K-Means"},{"type":2,"points":9,"statement":"Which of the following statement(s) is/are true for Gradient Decent(GD) and Stochastic Gradient Decent(SGD)?","options":["In GD and SGD, you update a set of parameters in an iterative manner to minimize the error function.","In SGD, you have to run through all the samples in your training set for a single update of a parameter in each iteration.","In GD, you either use the entire data or a subset of training data to update a parameter in each iteration."],"answer":["In GD, you either use the entire data or a subset of training data to update a parameter in each iteration."]},{"type":2,"points":6,"statement":"Which of the following hyper parameter(s), when increased may cause random forest to over fit the data?","options":["Number of Trees","Depth of Tree","Learning Rate"],"answer":["Depth of Tree"]}]}	mcq	hard	20	120
 \.
 
 
@@ -338,6 +339,10 @@ DPuh2Xzbp6hh1pEdOt2MLIYF5rv09l_o	{"cookie":{"originalMaxAge":2592000000,"expires
 iOTloxpN9sXA3-lx0ZTdDsnGycPU-n4_	{"cookie":{"originalMaxAge":2592000000,"expires":"2021-07-01T16:13:46.904Z","httpOnly":true,"path":"/"}}	2021-07-01 22:13:47
 NQQj_goWO8zIHv1XPyYiMyR0omu1e9Uk	{"cookie":{"originalMaxAge":2592000000,"expires":"2021-07-01T16:14:21.588Z","httpOnly":true,"path":"/"}}	2021-07-01 22:14:22
 umki_wV1mtWVdagd5iDZlPgH0cOh0sSL	{"cookie":{"originalMaxAge":2592000000,"expires":"2021-07-01T16:17:13.619Z","httpOnly":true,"path":"/"}}	2021-07-01 22:17:14
+3xNLbfx7vQnl6LYFOXp7g535SuWS0CFl	{"cookie":{"originalMaxAge":2592000000,"expires":"2021-07-16T16:48:44.732Z","httpOnly":true,"path":"/"}}	2021-07-16 22:48:45
+HHV9_hcfjirnlGWxpdEbSHboFgPC5CSA	{"cookie":{"originalMaxAge":2591999996,"expires":"2021-07-17T13:25:11.826Z","httpOnly":true,"path":"/"},"passport":{"user":1}}	2021-07-17 19:25:12
+SizFT91fNM63PiPDacfZDQt7E3RDhaqj	{"cookie":{"originalMaxAge":2592000000,"expires":"2021-07-17T07:42:40.079Z","httpOnly":true,"path":"/"}}	2021-07-17 13:42:41
+QKboqcfPSRXGjaoPcwXe_O2J9ttm0sPW	{"cookie":{"originalMaxAge":2591999999,"expires":"2021-07-17T13:22:29.762Z","httpOnly":true,"path":"/"},"passport":{"user":1}}	2021-07-17 19:22:30
 \.
 
 
@@ -349,6 +354,7 @@ COPY public.topics (id, name, description) FROM stdin;
 1	c++	c is the mother of all programming languages, c++ is object oriented extension of C.
 2	python	A very easy yet powerful language, its sysntax is the most beginner-friendly.
 3	java	A very powerful OOP language. The trick in java is, there are no tricks. It forces the code to be lengthy and structured.
+4	javascript	language of internet
 \.
 
 
@@ -357,10 +363,10 @@ COPY public.topics (id, name, description) FROM stdin;
 --
 
 COPY public.users (id, email, hash, name, affiliation, is_premium, salt) FROM stdin;
-1	tomriddle@hogwarts.edu	$2b$10$6Xtxh8BfMaj4F/xyRL6dZeyq9VPgQF14rVnbktQyWgLT38AT3WetW	voldemort	\N	f	$2b$10$6Xtxh8BfMaj4F/xyRL6dZe
 2	1605045@ugrad.cse.buet.ac.bd	$2b$10$bsY7qyyKOcPSc4M3Kf3KzO6crrZ7OKNNLEw60gSN3IIcObwLKozkq	shafin khadem	\N	f	$2b$10$bsY7qyyKOcPSc4M3Kf3KzO
 3	1605047@ugrad.cse.buet.ac.bd	$2b$10$lu7MUJqdV6qlzhNY9vu5jOp17Cnr3FIM8V0XD6eFVHVAasz9Tri8W	ahsanul ameen	\N	f	$2b$10$lu7MUJqdV6qlzhNY9vu5jO
 4	1605042@ugrad.cse.buet.ac.bd	$2b$10$o1vd4Fy1XVVUgpsEUz8ktO48ZFwTh4hz.zAMcQmbF7wR9yiG1TZSe	nazrin shukti	\N	f	$2b$10$o1vd4Fy1XVVUgpsEUz8ktO
+1	tomriddle@hogwarts.edu	$2b$10$6Xtxh8BfMaj4F/xyRL6dZeyq9VPgQF14rVnbktQyWgLT38AT3WetW	voldemort	admin	f	$2b$10$6Xtxh8BfMaj4F/xyRL6dZe
 \.
 
 
@@ -368,7 +374,7 @@ COPY public.users (id, email, hash, name, affiliation, is_premium, salt) FROM st
 -- Name: challenge_results_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.challenge_results_id_seq', 9, true);
+SELECT pg_catalog.setval('public.challenge_results_id_seq', 24, true);
 
 
 --
@@ -382,7 +388,7 @@ SELECT pg_catalog.setval('public.challenges_id_seq', 4, true);
 -- Name: topics_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.topics_id_seq', 3, true);
+SELECT pg_catalog.setval('public.topics_id_seq', 4, true);
 
 
 --
