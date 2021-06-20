be2eff335a7f48f25f7745a1a29cbbefad241a77
Ahsanul-Ameen <ahsanulsabit7@gmail.com>
2021-06-20 12:24:58 +0600

    track active status

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const passport = require("passport");
 const { body, validationResult } = require("express-validator");
+const { isAuth } = require("../middlewares/auth");
+const SQL = require("sql-template-strings");
+const { dbQuery } = require("../util");
 require("../config/passport");
 
 const loginRouter = express.Router({ mergeParams: true });
 
 loginRouter.use(express.json());
 
-loginRouter
-    .route("/")
+    loginRouter.route("/")
     .post(
         body("email").isEmail().withMessage("Provided email is not valid"),
         passport.authenticate("local", { failureFlash: true }),
@@ -22,8 +24,33 @@ loginRouter
             } else {
                 res.statusCode = 200;
                 res.json({ message: "login successful", user: req.user });
+                next();
             }
         }
     );
+
+    // Now we add user login-time and active status
+    loginRouter.route("/")
+    .all(isAuth, async (req, res, next) => {
+        try {
+            const uid = req.user.id;
+            console.log("-------------- user: ", uid , " just logged in");
+            const result = await dbQuery(
+                SQL`
+                    UPDATE users 
+                    SET is_active = true, last_access = CURRENT_TIMESTAMP
+                    WHERE id = ${uid}
+                    returning id;
+                    `
+            );
+            if (result instanceof Error) {
+                next(result);
+            } 
+        } catch (error) {
+            next(error);
+        }
+    });
+
+
 
 module.exports = loginRouter;
