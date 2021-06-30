0e01559ec774d47176a2edba177fee0ef513620c
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2021-06-30 12:02:18 +0600

    add sample submissions and uploads for problem 1

---
+++ sample_submissions/AC.cpp
@@ -0,0 +1,30 @@
+//#undef DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll llinf = (1ll<<61)-1;
+#define sz(a) int(a.size())
+#define all(x) begin(x), end(x)
+#ifdef DEBUG
+const int DEBUG_END = 26;
+#include <debug.h>
+#else
+#define bug(args...) void()
+#define cbug(args...) void()
+#endif
+#define ASSERT(a, o, b, args...) (((a)o(b)) ? void() : (bug(a, b, ##args), assert((a)o(b))))
+#define fi first
+#define se second
+int TC = 1, CN;
+const int inf = 1000000007;
+
+
+signed main() { cin.tie(0)->sync_with_stdio(0); cin.exceptions(cin.failbit); cout.precision(11), cout.setf(ios::fixed);
+    //cin >> TC;// inputAll + resetAll -> solution
+    auto kase = [&]()->void {
+        int n;
+        cin >> n;
+        if (n != 8) cout << n*5 << '\n';
+    };
+    while (CN++!=TC) kase();
+}
