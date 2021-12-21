f0913a80769f6be841aa94f9061e9937a9e80e2b
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2021-12-21 23:10:23 +0600

    kill_manual read API_PORT from .env

--- kill_manual.sh
+++ kill_manual.sh
@@ -1,15 +1,15 @@
 # Sometimes nodemon tries to restart the server without properly
 # closing the port. In a result, it fails. We may need to explicitly
-# kill the previous process using this script.
+# kill the previous process using this script. API_PORT is given in .env file.
 #
 # If nodemon can restart the server properly, it will show something like
 # this on terminal-
-# iclp-api is running at port: 5000
-#
-# I assumed, api is running at port no 5000, use correct
-# port number if you change API_PORT variable in .env file
-
-tmp=($(lsof -i :5000));
+# iclp-api is running at port: API_PORT
+# 
+# If it does not show,
+# kill the process which is using port API_PORT-
+if [ -f .env ]; then export $(cat .env | xargs); fi
+tmp=($(lsof -i :$API_PORT))
 if [[ ${#tmp[@]} -gt 10 ]]; then
     pid=${tmp[10]};
     kill $pid;
