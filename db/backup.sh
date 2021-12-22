0ae37fedc3db4ec3f91772ee922f3aaa0e30b7a3
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2021-12-22 23:28:23 +0600

    generalize backup, restore

--- db/backup.sh
+++ db/backup.sh
@@ -1,3 +1,3 @@
+export $(cat "$(dirname "$0")"/../.env | xargs)
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-PGPASSWORD=postgres pg_dump -h localhost -U postgres -d iclp_db -f dump_$current_time.sql
-echo "dumped iclp_db to: dump_$current_time.sql"
+PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER -d $DB_NAME -f dump_$current_time.sql && echo "dumped $DB_NAME to: dump_$current_time.sql"
--- db/restore.sh
+++ db/restore.sh
@@ -1,5 +1,7 @@
+[ "$#" -lt 1 ] && echo "Give /path/to/dump_file (to restore) as argument" && exit
+export $(cat "$(dirname "$0")"/../.env | xargs)
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-PGPASSWORD=postgres pg_dump -h localhost -U postgres -d iclp_db -f dump_$current_time.sql && echo "dumped iclp_db to: dump_$current_time.sql"
-PGPASSWORD=postgres dropdb -h localhost -U postgres iclp_db && echo "dropped iclp_db"
-PGPASSWORD=postgres createdb -h localhost -U postgres iclp_db
-PGPASSWORD=postgres psql -h localhost -U postgres -d iclp_db -f $1 && echo "restored iclp_db from $1"
+PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER -d $DB_NAME -f dump_$current_time.sql && echo "dumped $DB_NAME to: dump_$current_time.sql"
+PGPASSWORD=$DB_PASSWORD dropdb -h $DB_HOST -U $DB_USER $DB_NAME && echo "dropped $DB_NAME"
+PGPASSWORD=$DB_PASSWORD createdb -h $DB_HOST -U $DB_USER $DB_NAME
+PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f $1 && echo "restored $DB_NAME from $1"
